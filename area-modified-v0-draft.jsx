1dafea990802e8ed578971877c788499895a9b59
xaei <this@e.local>
2013-12-21 11:49:35 -0800

    added scale lines and cleaned up rounding

---
+++ area-modified-v0-draft.jsx
@@ -0,0 +1,25 @@
+//additions for scaling 131221-112724 AM
+//based on JET Get Area
+
+if ( app.documents.length > 0 && app.activeDocument.selection.length > 0 ) {
+	var docRef=app.activeDocument;
+		
+//	for (var i in docRef.selection){
+		var pathRef=docRef.selection[0];
+		var pathRefArea=pathRef.area;
+		var pathAreaHumanReadable=(Math.round(Math.abs(pathRefArea))).toFixed(2);
+		var pathAreaInches=(Math.round(pathAreaHumanReadable/5184)).toFixed(2);
+
+		alert("raw area: "+(pathAreaHumanReadable)+"\n"+
+			"1:1 scale: "+(pathAreaInches)+" sq. in.\n"+
+			"3/32 scale: "+((pathAreaInches*113.78)).toFixed(2)+" sq. ft.\n"+
+			"3/16 scale: "+((pathAreaInches*28.444)).toFixed(2)+" sq. ft.\n"+
+			"1/4 scale: "+((pathAreaInches*16)).toFixed(2)+" sq. ft.\n");
+
+		// alert ("Square Points: "+Math.round(pathRefArea*100)/100+"\n"
+		// +"Square Picas: "+(Math.round((pathRefArea/144)*100))/100+"\n"
+		// +"Square Inches: "+(Math.round((pathRefArea/5184)*100))/100+"\n"
+		// +"Square Millimeters: "+(Math.round((pathRefArea/8.037)*100))/100+"\n"
+		// +"Square Centimeters: "+(Math.round((pathRefArea/803.520)*100))/100);
+		
+	}	
